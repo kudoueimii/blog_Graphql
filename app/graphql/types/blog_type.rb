9ba058d4bfa709fe20dd872bff0d31b295766ab9
bargholz eimii <818_77_@ezweb.ne.jp>
2022-11-12 14:37:35 +0900

    space first

--- app/graphql/types/blog_type.rb
+++ app/graphql/types/blog_type.rb
@@ -1,9 +1,9 @@
-# frozen_string_literal: true
-
 module Types
   class BlogType < Types::BaseObject
     field :id, ID, null: false
     field :title, String, null: false
     field :text, String, null: false
+    field :created_at, GraphQL::Types::IS08601DateTime, nill: false
+    field :updated_at, GraphQL::Types::IS08601DateTime, nill: false
   end
 end
